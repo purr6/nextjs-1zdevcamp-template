9080c3236a4aafb5af479538c341ec61545e0309
Ken <ken@1zlabs.com>
2024-01-16 18:32:19 +0900

    Add @radix-ui/react-label component and create NewMemory component

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from 'react'
+import { NewMemory } from '@/components/new-memory';
 
-
-export default function NewMemory() {
+export default function NewMemoryPage() {
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+            <NewMemory />
         </div>
     )
 }
