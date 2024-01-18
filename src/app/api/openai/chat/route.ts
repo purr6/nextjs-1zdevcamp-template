e699c9039b72fd88a30d7800a73ebf740661d83e
Ken <ken@1zlabs.com>
2024-01-18 11:19:12 +0900

    Refactor chat route to include system message and convert memories to string

--- src/app/api/openai/chat/route.ts
+++ src/app/api/openai/chat/route.ts
@@ -17,6 +17,18 @@ export async function POST(req: Request) {
   const memories = await prisma.chatMemory.findMany({});
   console.log(memories);
 
+  //memories are json array now. make memories_str title and content to string
+  var memories_str = "";
+  for (var i = 0; i < memories.length; i++) {
+    memories_str += `(${memories[i].title}:${memories[i].content}),`;
+  }
+
+  //add system message to messages array first position
+  //{ "role": "system", "content": "content"} 
+  messages.unshift({ "role": "system", "content": `아래는 유저와 대화하며 기억 및 기록한 내용이야. 이 기억을 참조해서 답변을 해줘, [${memories_str}]` });
+
+  console.log(messages);
+
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
